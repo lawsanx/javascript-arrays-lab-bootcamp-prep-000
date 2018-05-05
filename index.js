7e20891c2db9ef76f74c77ed34f8b9a0ce6b4bf2
sailboatlarry@gmail.com <sailboatlarry@gmail.com>
2018-05-05 15:55:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,7 @@ function destructivelyPrependKitten(name){
 function destructivelyRemoveLastKitten(name){
   return kittens.pop()
 }
+
+function destructivelyRemoveFirstKitten(name){
+  return kittens.shift()
+}
